f94ebbdcd476a5722853469492f4ce2fcd5b33b6
Andy Kirkland <andykirkland@outlook.com>
2025-12-05 22:32:23 -0500

    removes

--- apply_monthly_income_azure.sql
+++
@@ -1,9 +0,0 @@
--- Add MonthlyIncome column to Azure database
-ALTER TABLE [dbo].[Customers]
-ADD [MonthlyIncome] decimal(10,2) NULL;
-
--- Add migration record
-INSERT INTO [dbo].[__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20250111000000_AddMonthlyIncomeToCustomer', N'10.0.0');
-
-SELECT 'MonthlyIncome column added successfully!' AS Result;
